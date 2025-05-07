a7e6cd92b097a54c0fba1fbf2a2746105923a3e4
sadew <www.sadewthegrade@gmail.com>
2025-05-07 10:30:06 +0530

    simple data types using with arithmetics operators

--- python2025Day01.py
+++ python2025Day01.py
@@ -1,9 +1,12 @@
-#arithmatic operators
-
 print (5+2)
-print (5-2)
-print (5*2)
-print (5/2)
-print (5//2)
-print (5%2)
-print (5**2)
+print (5+2.0)
+print (5.0 * True)
+print (5 - True)
+
+#special situation
+
+print (5 / True)
+print (6 / 2)
+print (True / True)
+
+
