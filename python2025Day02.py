1117eb0b02ab2b7176ea2f3e387bde229b7d0370
sadew <www.sadewthegrade@gmail.com>
2025-05-07 11:50:23 +0530

    arithmetic operators using negative & positive numbers

--- python2025Day02.py
+++ python2025Day02.py
@@ -1,7 +1,13 @@
-#print (5+4/2)
-#print ((5+4)/2)
 
 #print (2(6))bracket is not a operator
 
-print (2**3**2)
-print (2*4/2/3)
+print (+5++2)
+print (-5+-2)
+print (+5+-2)
+print (-5++2)
+
+print ((-1)**1)
+print ((-1)**2)
+print ((-1)**3)
+print ((-1)**4)
+
